1bcfcadfef496f59e55fced9d9d93683d00b5fac
Luna Sarmiento <selene942008@hotmail.com>
2021-08-20 18:02:43 +0200

    Update FetchMovies.jsx

--- netflix/src/components/FetchMovies.jsx
+++ netflix/src/components/FetchMovies.jsx
@@ -15,7 +15,11 @@ class FetchMovies extends Component {
         "https://strive-netflix-api-luna.herokuapp.com/media"
       );
       if (resp.ok) {
-        console.log(resp.json());
+        const movies = await resp.json();
+        this.setState({
+          arrMovies: movies,
+        });
+        console.log(arr);
       } else {
         let response = await fetch(
           "http://www.omdbapi.com/?i=tt3896198&apikey=e7302d6b&s="
